db6ecc4b0ba2d64661dd02188b316cb0a0e88115
nosedive25 <nosedivesoftware@me.com>
2013-11-07 22:28:09 -0500

    Working Base
    Working base completed, implimented:
    
    - Error handeling for modules
    - Basic module API
    - Metrics
    - Error manager
    - Arduino interface
    - Basic CLI

--- FishTank.py
+++ FishTank.py
@@ -1,18 +1,100 @@
 import os
 import sys
 import threading
+import serial
 
 sys.path.insert(0, "modules/")
 
-dir = os.listdir("modules/")
-for mod in dir:
+mods = []
+arduino = None
+metrics = {}
+errors = {}
+kErrors = {}
+modsDir = os.listdir("modules/")
+
+#Send Arduino Message        
+def sendArduinoMessage(mssg):
+    arduino.write(mssg)
+    
+#Connect to an Arduino board    
+def connectToArduino(dev, rate):
+    arduino = serial.Serial(dev, rate)
+    
+    while True:
+        strIn = arduino.readline()
+        for mod in mods:
+            if strIn.beginswith("(" + mod.moduleName() + ")"):
+                mod.incomingArduinoMessage(strIn.replace("(" + mod.moduleName() + ")", ""))
+
+#Add a metric                
+def addMetric(key, value, mod):
+    metrics[mod + "." + key] = value;
+
+#Get a metric    
+def getMetric(key, mod):
+    return metrics[mod + "." + key];
+    
+#Report an error    
+def sendError(error, level, mod):
+    errors[mod + "." + error] = level
+    if level == kErrors["eCritical"]:
+        pass
+    
+#Sends an SMS to the specified recipiant
+def sendSMS(mssg, recipiant):
+    pass
+    
+#Sends an SMS to the specified recipiant
+def checkForIncomingSMS():
+    pass
+    
+#Configure error levels
+kErrors["eWarning"] = 0
+kErrors["eStandard"] = 1
+kErrors["eCritical"] = 2
+
+#Load all other modules
+for mod in modsDir:
     if mod != ".DS_Store" and ".pyc" not in mod:
         _mod = __import__(mod.replace(".py", ""))
-        print "Loaded " + mod.replace(".py", "")  + ", version " + _mod.moduleVersion() + " by " +  _mod.moduleAuthor()
-        _mod.moduleInit()
         
-        modThread = threading.Thread(target=_mod.moduleRun())
-        modThread.start()
-        
-def sendArduinoMessage(mssg):
-    print "Function not implimented!"
+        if getattr(_mod, 'moduleInit', None) is None or getattr(_mod, 'moduleRun', None) is None or getattr(_mod, 'moduleVersion', None) is None or getattr(_mod, 'moduleAuthor', None) is None or getattr(_mod, 'incomingArduinoMessage', None) is None or getattr(_mod, 'moduleDescription', None) is None or getattr(_mod, 'moduleName', None) is None:
+            print "Error loading module: " + mod + ". The module is not configured correctly."
+        else:
+            try:
+                print "Loaded " + mod.replace(".py", "")  + ", version " + _mod.moduleVersion() + " by " +  _mod.moduleAuthor()
+            except:
+                print sys.exc_info()[0]
+                sendError("Access on Version or Author failure", kErrors["eStandard"], mod.replace(".py", ""))
+            
+            try:
+                _mod.moduleInit(addMetric, getMetric, sendError, sendArduinoMessage, kErrors)
+            except:
+                print sys.exc_info()[0]
+                sendError("Init() function failed to execute", kErrors["eCritical"], mod.replace(".py", ""))
+            
+            try:
+                modThread = threading.Thread(target=_mod.moduleRun())
+                modThread.start()
+            except:
+                print sys.exc_info()[0]
+                sendError("Run() function failed to execute", kErrors["eCritical"], mod.replace(".py", ""))
+            
+            mods.append(_mod)
+
+smsThread = threading.Thread(target=checkForIncomingSMS)
+smsThread.start()
+#Connect to the Arduino board on a seperate thread
+#arduinoThread = threading.Thread(target=connectToArduino("", 9600))
+#arduinoThread.start()
+
+while True:
+    cmd = raw_input("FishTank$ ")
+    if cmd == "exit":
+        sys.exit(0)
+    elif cmd == "metrics":
+        print metrics
+    elif cmd == "errors":
+        print errors
+    else:
+       print "Unrecognized command"
--- modules/TestModule.py
+++ modules/TestModule.py
@@ -1,18 +1,51 @@
 import sys
 import os
 
+
+##API##
+#Note: All functions must be implimented and must retain standard arguments.
+#If a module fails to do this, it will not be loaded.
+#
+#sendArduinoMessage(mssg) - Sends a message to the connected Arduino board
+#addMetric(key, value, mod) -  Adds a metric to the FishTank, accessable to all modules.
+#getMetric(key, value, mod) - Gets a metric from the FishTank about the module <mod>
+#sendError(error, level, mod) - Reports an error that occured in the module. 
+#
+#Error Levels:
+#   error[eWarning] - An error that causes little to no harm
+#   error[eStandard] - An error that will likely cause harm if left unchecked for a long period of time
+#   error[eCritical] - An error that will trigger an SMS message, an error that must be corrected immediatly
+
+addMetric = None
+getMetric = None
+sendError = None
+sendArduinoMessage = None
+error = None
+
 #Initialize the module, must not cause a thread lock
-def moduleInit(): 
+def moduleInit(am, gm, se, sam, errs): 
+    #Do not modify below#
+    addMetric = am
+    getMetric = gm
+    sendError = se
+    sendArduinoMessage = sam
+    error = errs
+    #Do not modify above#
+
     print "Test init completed"
 
 #Run the module, usually a loop  
 def moduleRun():
-    sendArduinoMessage("hello")
     print "Test run started"
+   
     
 #Returns the author of the module    
 def moduleAuthor():
-    return "Test"
+    return "John Smith"
+    
+#Returns the name of the module    
+def moduleName():
+    return "TestModule"
     
 #Returns the module verison
 def moduleVersion():
