8a0f334f9b58e9ec5ec4775d4c49b6ded4b191c2
nosedive25 <nosedivesoftware@me.com>
2013-11-10 23:11:20 -0500

    Lots of Fixes
    - Better CLI with more commands
    - Overhauled threading
    - Started SMS system
    - Reworked Arduino messaging system
    - Removed incomingArduinoMessage() in favor of a Queue based system
    - Added a logging system, log()

--- FishTank.py
+++ FishTank.py
@@ -1,30 +1,44 @@
 import os
 import sys
-import threading
+import thread
 import serial
+import threading
+import base64
+import imaplib
+import getpass
+import Queue
+import re
 
 sys.path.insert(0, "modules/")
 
 mods = []
 arduino = None
+arduinoDevice = None
 metrics = {}
 errors = {}
 kErrors = {}
 modsDir = os.listdir("modules/")
+mailPass = None
+printQueue = Queue.Queue()
+writeQueue = Queue.Queue()
+
+##TODO##
+#Finish SMS implimentation
+#Fix the log() function
 
 #Send Arduino Message        
 def sendArduinoMessage(mssg):
-    arduino.write(mssg)
+    writeQueue.put(base64.b64encode(mssg))
     
 #Connect to an Arduino board    
 def connectToArduino(dev, rate):
     arduino = serial.Serial(dev, rate)
     
     while True:
-        strIn = arduino.readline()
+        strIn = base64.b64decode(arduino.readline())
         for mod in mods:
             if strIn.beginswith("(" + mod.moduleName() + ")"):
-                mod.incomingArduinoMessage(strIn.replace("(" + mod.moduleName() + ")", ""))
+                mod.incomingQueue.put(strIn.replace("(" + mod.moduleName() + ")", ""))
 
 #Add a metric                
 def addMetric(key, value, mod):
@@ -34,31 +48,56 @@ def addMetric(key, value, mod):
 def getMetric(key, mod):
     return metrics[mod + "." + key];
     
+#Logging for modules             
+def log(mod, mssg):
+    printQueue.put("[" + mod + "] "+ mssg)
+    
 #Report an error    
 def sendError(error, level, mod):
     errors[mod + "." + error] = level
     if level == kErrors["eCritical"]:
-        pass
+        sendSMS(mod + "." + error, "")
     
 #Sends an SMS to the specified recipiant
 def sendSMS(mssg, recipiant):
     pass
     
-#Sends an SMS to the specified recipiant
+#Reads from the incoming IMAP server
 def checkForIncomingSMS():
-    pass
+     mailserver = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+     mailserver.login("fishtank@nosedivesoftware.com", mailPass)
+     mailserver.select('INBOX')
+     stat, ids = mailserver.uid('search', None, "ALL")
+     for id in ids[0].split(" "):
+        result, email = mailserver.uid('fetch', id, '(RFC822)')
+        if "saltyfish" in email[0][1]:
+            sender = re.compile("From: .*").search(email[0][1]).group()[0]
+            print sender
+     
+#Write out any queued messages for Arduino     
+def writeQueueToBoard():
+    while True:
+        i = 0
+        while i < writeQueue.qsize():
+            arduino.write(writeQueue.get())
+            writeQueue.task_done()
     
 #Configure error levels
 kErrors["eWarning"] = 0
 kErrors["eStandard"] = 1
 kErrors["eCritical"] = 2
 
+#Get mail information
+
+while mailPass == "" or mailPass == " " or mailPass == None:
+    mailPass = getpass.getpass("Mail Password:")
+
 #Load all other modules
 for mod in modsDir:
     if mod != ".DS_Store" and ".pyc" not in mod:
         _mod = __import__(mod.replace(".py", ""))
         
-        if getattr(_mod, 'moduleInit', None) is None or getattr(_mod, 'moduleRun', None) is None or getattr(_mod, 'moduleVersion', None) is None or getattr(_mod, 'moduleAuthor', None) is None or getattr(_mod, 'incomingArduinoMessage', None) is None or getattr(_mod, 'moduleDescription', None) is None or getattr(_mod, 'moduleName', None) is None or getattr(_mod, 'stopModule', None) is None:
+        if getattr(_mod, 'moduleInit', None) is None or getattr(_mod, 'moduleRun', None) is None or getattr(_mod, 'moduleVersion', None) is None or getattr(_mod, 'moduleAuthor', None) is None or getattr(_mod, 'moduleDescription', None) is None or getattr(_mod, 'moduleName', None) is None or getattr(_mod, 'stopModule', None) is None:
             print "Error loading module: " + mod + ". The module is not configured correctly."
         else:
             try:
@@ -68,25 +107,22 @@ for mod in modsDir:
                 sendError("Access on Version or Author failure", kErrors["eStandard"], mod.replace(".py", ""))
             
             try:
-                _mod.moduleInit(addMetric, getMetric, sendError, sendArduinoMessage, kErrors)
+                _mod.moduleInit(addMetric, getMetric, sendError, sendArduinoMessage, log, kErrors)
             except:
-                sendError("Init():" + sys.exc_info()[0], kErrors["eCritical"], mod.replace(".py", ""))
+                sendError("Init():" + str(sys.exc_info()[0]), kErrors["eCritical"], mod.replace(".py", ""))
             
             try:
-                modThread = threading.Thread(target=_mod.moduleRun())
-                modThread.start()
+                thread.start_new_thread(_mod.moduleRun, (addMetric, getMetric, sendError, sendArduinoMessage, log, kErrors))
             except:
-                sendError("Run():" + sys.exc_info()[0], kErrors["eCritical"], mod.replace(".py", ""))
+                sendError("Run():" + str(sys.exc_info()[0]), kErrors["eCritical"], mod.replace(".py", ""))
             
             mods.append(_mod)
 
-smsThread = threading.Thread(target=checkForIncomingSMS)
-smsThread.start()
+thread.start_new_thread(checkForIncomingSMS, ())
+thread.start_new_thread(writeQueueToBoard, ())
 
-#Connect to the Arduino board on a seperate thread
-arduinoThread = threading.Thread(target=connectToArduino("/dev/tty.usbserial-A6027N04", 9600))
-arduinoThread.start()
-
+#Run loop for CLI
+print "\n"
 while True:
     cmd = raw_input("FishTank$ ")
     if cmd == "exit":
@@ -97,5 +133,46 @@ while True:
         print metrics
     elif cmd == "errors":
         print errors
+    elif "set" in cmd:
+        if "arduino" in cmd:
+            arduinoDevice = cmd.replace("set arduino ", "")
+        else:
+            print "set: Invalid variable name"
+    elif cmd == "mods" or cmd == "modules":
+        for mod in mods:
+            print mod.moduleName()
+    elif "connect" in cmd:
+        if "arduino" in cmd and not arduinoDevice == None:
+            thread.start_new_thread(connectToArduino, (arduinoDevice, 9600))
+    elif "stop" in cmd:
+        for mod in mods:
+            if mod.moduleName() == cmd.replace("stop ", ""):
+                mod.stopModule()
+    elif cmd == "help":
+        print "FishTank Help"
+        print "============="
+        print "metrics: displays the avalible metrics from loaded modules"
+        print "errors: displays any reported errors from loaded modules"
+        print "stop <module name>: stops the specified module"
+        print "set <var name> <value>: sets the given variable to the specified value"
+        print "connect <device name>: initiates a connection to the specified device"
+        print "list <devices | variables>: lists the avalible devices or variables"
+        print "exit: requests that all modules stop and quits FishTank"
+    elif "list" in cmd:
+        if "devices" in cmd:
+            if not arduinoDevice == None:
+                print "'arduino': " + arduinoDevice
+        elif "variables" in cmd:
+            if arduinoDevice == None:
+                print "arduino: not set"
+            else:
+                 print "arduino: " + arduinoDevice
+        else:
+            print "list: Invalid item to list"
+    elif cmd == "log":
+        i = 0
+        while i < printQueue.qsize():
+            print printQueue.get()
+            printQueue.task_done()
     else:
        print "Unrecognized command"
--- modules/TestModule.py
+++ modules/TestModule.py
@@ -1,15 +1,16 @@
 import sys
 import os
-
+import Queue
 
 ##API##
 #Note: All functions must be implimented and must retain standard arguments.
 #If a module fails to do this, it will not be loaded.
 #
-#sendArduinoMessage(mssg) - Sends a message to the connected Arduino board
+#sendArduinoMessage(mssg) - Sends a message to the connected Arduino board. You MUST impliment the writeLock to prevent concurrent usage of this function!
 #addMetric(key, value, mod) -  Adds a metric to the FishTank, accessable to all modules.
 #getMetric(key, value, mod) - Gets a metric from the FishTank about the module <mod>
 #sendError(error, level, mod) - Reports an error that occured in the module. 
+#log(mod, mssg) - Prints a message to the main console
 #
 #Error Levels:
 #   error[eWarning] - An error that causes little to no harm
@@ -21,23 +22,38 @@ getMetric = None
 sendError = None
 sendArduinoMessage = None
 error = None
+log = None
+incomingQueue = Queue.Queue()
 
-#Initialize the module, must not cause a thread lock
-def moduleInit(am, gm, se, sam, errs): 
+#Initialize the module, must not cause a lock
+def moduleInit(am, gm, se, sam, lg, errs): 
     #Do not modify below#
     addMetric = am
     getMetric = gm
     sendError = se
     sendArduinoMessage = sam
     error = errs
+    log = lg
     #Do not modify above#
-
-    print "Test init completed"
+    
+    log(moduleName(), "Test init completed")
 
 #Run the module, usually a loop  
-def moduleRun():
-    print "Test run started"
-   
+def moduleRun(am, gm, se, sam, lg, errs):
+    #Do not modify below#
+    addMetric = am
+    getMetric = gm
+    sendError = se
+    sendArduinoMessage = sam
+    error = errs
+    log = lg
+    #Do not modify above#
+    
+    log(moduleName(), "Test run started")
+    
+    #Add your main loop code below
+    while True:
+        pass
     
 #Returns the author of the module    
 def moduleAuthor():
@@ -54,11 +70,7 @@ def moduleVersion():
 #Returns a descripton of the module    
 def moduleDescription():
     return "A test module"
-    
-#Called when a connected Arduino board delivers serial data with to the module
-def incomingArduinoMessage(mssg):
-    print "Serial message recived: " + mssg
 
 #Called when the module is stopped
 def stopModule():
-    print "Module stopped"
+    pass
